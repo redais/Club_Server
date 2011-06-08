d554834a554513c439c83921d46de05a477e9e04
Redais <Redais@10.100.18.100>
2011-06-08 17:22:19 +0200

    Validatation: first_name, last_name, email, password

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,30 +5,31 @@ class Member < ActiveRecord::Base
   include Authentication::ModelInstanceMethods
   
   has_and_belongs_to_many :clubs
-  attr_accessible :first_name, :last_name, :sex, :email, :city ,:address, :postale_code, :birthday, :chip_id, :password, :password_confirmation
   
-  validates :club_id, :presence => true   
-  validates :email, :format     => { :with => Authentication.email_regex, :message => Authentication.bad_email_message },
-                    :presence   => true,
-                    :uniqueness => true,
-                    :length     => { :within => 20..40 }
-                   
-
-  validates :last_name,  :format     => { :with => Authentication.name_regex, :message => Authentication.bad_name_message },
-                    :length     => { :maximum => 50 },
-                    :allow_nil  => true,
-                    :presence   => true
-                    
+  set_table_name 'members'
+  
+  validates :last_name,   :format     => { :with => Authentication.name_regex, :message => Authentication.bad_name_message },
+                          :length     => { :maximum => 50 },
+                          :allow_nil  => true,
+                          :presence   => true
+                 
   validates :first_name,  :format     => { :with => Authentication.name_regex, :message => Authentication.bad_name_message },
-                    :length     => { :maximum => 50 },
-                    :allow_nil  => true,
-                    :presence   => true
-                    
-  validates :password, :presence     => true,
-                       :confirmation => true,
-                       :length       => { :within => 6..40 }
+                          :length     => { :maximum => 50 },
+                          :allow_nil  => true,
+                          :presence   => true
+                          
+  validates :email,       :format     => {:with => Authentication.email_regex, :message => Authentication.bad_email_message},
+                          :length     => {:maximum => 50},
+                          :presence   => true
+  
+  validates :password,    :presence     => true,
+                          :confirmation => true,
+                          :length       => { :within => 6..40 }
+                       
   
   
+  
+  attr_accessible :first_name, :last_name, :sex, :email, :city ,:address, :postale_code, :birthday, :chip_id, :password, :password_confirmation
   # Authenticates a club_member by their email and unencrypted password.  Returns the member or nil.
   #
   # uff.  this is really an authorization, not authentication routine.  
